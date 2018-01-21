ca02a49e17bfe7d4fb735e98bd08e73203ce77d4
souleymane98 <fallsouleymane22@gmail.com>
2018-01-21 16:59:27 +0100

    Update lecture.py

--- partie1/lecture.py
+++ partie1/lecture.py
@@ -3,7 +3,7 @@
 import serial
 
 #Pour mac le serial va chercher votre carte sur se chemin /dev/tty.usbserial donc au lieu de mettre COM1 vous allez mettre (/dev/tty.usbserial)
-ser = serial.Serial('COM1', 9600)
+serialconnect = serial.Serial('COM1', 9600)
 while True:
-    print (ser.readline())
+    print (serialconnect.readline())
 #Print ser.readline  va afficher le messsage que vous avez envoyer sur le serial
